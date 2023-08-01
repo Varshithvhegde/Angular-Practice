6e57ce0384f9d2dae827454e6cc44d607b7c0dad
Varshithvhegde <varshithvh@gmail.com>
2023-08-01 22:08:27 +0530

    Ng style ng if nf for ng class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import {FormsModule } from "@angular/forms";
 
+interface Pokemon{
+  id : number,
+  name : String,
+  type : String,
+  isCool :boolean,
+  isStylish : boolean
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  title :string="Basket of Pokemon";
+  imgSrc: string ="https://iheartcraftythings.com/wp-content/uploads/2019/04/pokemon-easter-basket-8.jpg"
+  alt:string ="None"
+  favoriteAnimal :String  = "Lion";
+
+  pokemonName : string ="";
+  constructor(){
+  
+  }
+  handleClick(value :any){
+    console.log(value);
+  }
+
+  pokemons : Pokemon[]=[{
+    id:1,
+    name:"Pikachu",
+    type :"Eletric",
+    isCool :false,
+    isStylish : true
+  },{
+    id:2,
+    name : "IceEdge",
+    type :"Ice",
+    isCool : true,
+    isStylish : false
+  },{
+    id:3,
+    name:"Eeve",
+    type :"Normal",
+    isCool :true,
+    isStylish : true
+  }]
+
 }
