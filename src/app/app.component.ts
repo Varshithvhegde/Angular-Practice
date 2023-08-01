ec4360032f5f725a04456550af4a94899f54fa57
Varshithvhegde <varshithvh@gmail.com>
2023-08-01 22:28:18 +0530

    ngIF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,37 +16,41 @@ interface Pokemon{
 })
 export class AppComponent {
 
-  title :string="Basket of Pokemon";
-  imgSrc: string ="https://iheartcraftythings.com/wp-content/uploads/2019/04/pokemon-easter-basket-8.jpg"
-  alt:string ="None"
-  favoriteAnimal :String  = "Lion";
+  // title :string="Basket of Pokemon";
+  // imgSrc: string ="https://iheartcraftythings.com/wp-content/uploads/2019/04/pokemon-easter-basket-8.jpg"
+  // alt:string ="None"
+  // favoriteAnimal :String  = "Lion";
 
   pokemonName : string ="";
   constructor(){
   
   }
-  handleClick(value :any){
-    console.log(value);
-  }
+  // handleClick(value :any){
+  //   console.log(value);
+  // }
 
-  pokemons : Pokemon[]=[{
-    id:1,
-    name:"Pikachu",
-    type :"Eletric",
-    isCool :false,
-    isStylish : true
-  },{
-    id:2,
-    name : "IceEdge",
-    type :"Ice",
-    isCool : true,
-    isStylish : false
-  },{
-    id:3,
-    name:"Eeve",
-    type :"Normal",
-    isCool :true,
-    isStylish : true
-  }]
+  // pokemons : Pokemon[]=[{
+  //   id:1,
+  //   name:"Pikachu",
+  //   type :"Eletric",
+  //   isCool :false,
+  //   isStylish : true
+  // },{
+  //   id:2,
+  //   name : "IceEdge",
+  //   type :"Ice",
+  //   isCool : true,
+  //   isStylish : false
+  // },{
+  //   id:3,
+  //   name:"Eeve",
+  //   type :"Normal",
+  //   isCool :true,
+  //   isStylish : true
+  // }]
+
+  handleChange(event : any){
+    this.pokemonName = event.target.value;
+  }
 
 }
