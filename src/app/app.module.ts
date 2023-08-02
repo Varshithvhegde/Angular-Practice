2b3d483e5d0bf153b4c7e62b3bd26f8c9119a757
Varshithvhegde <varshithvh@gmail.com>
2023-08-02 10:47:14 +0530

    Creating and Adding Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonBaseModule } from './pokemon-base/pokemon-base.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    PokemonBaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pokemon-base/pokemon-base.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokemonListComponent
+  ],
+  imports: [
+    CommonModule,
+    
+  ],
+  exports : [
+    PokemonListComponent
+  ]
+})
+export class PokemonBaseModule { }
