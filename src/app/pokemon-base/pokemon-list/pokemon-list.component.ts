4486e97c32a9f8d5e8ded669f2f02a8c4dedc531
Varshithvhegde <varshithvh@gmail.com>
2023-08-02 11:34:45 +0530

    @Input Example Added

--- src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-base/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
-export class PokemonListComponent {
-
+export class PokemonListComponent implements OnInit {
+  pokemons: Pokemon[] = [
+    {
+      id: 1,
+      name: 'Pikachu',
+      type: 'Electric',
+      isCool: true,
+    },
+    {
+      id: 2,
+      name: 'Charmander',
+      type: 'Fire',
+      isCool: false,
+    },
+    {
+      id: 3,
+      name: 'Eeve',
+      type: 'Normal',
+      isCool: true,
+    },
+  ];
+  constructor() {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
